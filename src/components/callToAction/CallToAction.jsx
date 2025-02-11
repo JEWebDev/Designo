4acbd2aa71f7e609ce9cb11f0dd014f34bba173a
Juan Esparza <83143208+JEWebDev@users.noreply.github.com>
2025-02-11 12:56:21 -0600

    Fix visual details of call to action and footer components

--- src/components/callToAction/CallToAction.jsx
+++ src/components/callToAction/CallToAction.jsx
@@ -4,11 +4,13 @@ export function CallToAction() {
   return (
     <section className="call-to-action-section">
       <div className="content">
-        <h3>Lets Talk about your project</h3>
-        <p>
-          Ready to take it to the next level? Contact us today and find out how
-          our expertise can help your business grow.
-        </p>
+        <div className="text">
+          <h3>Lets Talk about your project</h3>
+          <p>
+            Ready to take it to the next level? Contact us today and find out
+            how our expertise can help your business grow.
+          </p>
+        </div>
         <MainButton theme="light" link="#">
           get in touch
         </MainButton>
